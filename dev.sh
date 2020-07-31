5cd420a28f2a3907c72555ed1595acd8adaffdd8
Franky <hoangtuan1501@gmail.com>
2020-07-31 16:23:34 +0700

    Build linux binary with with sqlite embedded

--- dev.sh
+++ dev.sh
@@ -5,17 +5,18 @@ NC='\033[0m' # No Color
 
 build() {
   printf "${RED}Building binary...${NC}\n"
-  CGO_ENABLED=0 GOOS=linux go build -o bin/centmonit-linux
-  CGO_ENABLED=0 GOOS=solaris go build -o bin/centmonit-solaris
-  CGO_ENABLED=0 GOOS=openbsd go build -o bin/centmonit-openbsd
-  CGO_ENABLED=0 GOOS=netbsd go build -o bin/centmonit-netbsd
-  CGO_ENABLED=0 GOOS=freebsd go build -o bin/centmonit-freebsd
+  CGO_ENABLED=1 GOOS=linux go build -o bin/centmonit-linux
+  # CGO_ENABLED=0 GOOS=solaris go build -o bin/centmonit-solaris
+  # CGO_ENABLED=0 GOOS=openbsd go build -o bin/centmonit-openbsd
+  # CGO_ENABLED=0 GOOS=netbsd go build -o bin/centmonit-netbsd
+  # CGO_ENABLED=0 GOOS=freebsd go build -o bin/centmonit-freebsd
 }
 
 docker_deployment() {
   printf "${RED}Deploy to docker test...${NC}\n"
   CONTAINER=test_monit
   docker cp bin $CONTAINER:/home/CentMonit/
+  # docker cp db $CONTAINER:/home/CentMonit/
   docker cp html $CONTAINER:/home/CentMonit/
   docker cp template $CONTAINER:/home/CentMonit/
   docker cp config.yml $CONTAINER:/home/CentMonit/
